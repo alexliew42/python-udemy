abc985c3f8c976e36a6c8018d4b053693f760132
Alex Liew <liewtwoo42@gmail.com>
2023-07-31 13:56:01 -0400

    squared numbers 235

--- day26/day26.py
+++ day26/day26.py
@@ -7,11 +7,16 @@
 # doubled_numbers = [n * 2 for n in range(1, 5)]
 # print(doubled_numbers)
 
-names = ['Alex', 'Beth', "Caroline", 'Dave', 'Eleanor', 'Freddie']
+# names = ['Alex', 'Beth', "Caroline", 'Dave', 'Eleanor', 'Freddie']
 
-# short_names = [short for short in names if len(short) <= 4]
-# print(short_names)
+# # short_names = [short for short in names if len(short) <= 4]
+# # print(short_names)
 
-long_names = [name.upper() for name in names if len(name) > 5]
+# long_names = [name.upper() for name in names if len(name) > 5]
 
-print(long_names)
+# print(long_names)
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+squared_numbers = [num * num for num in numbers]
+
+print(squared_numbers)
