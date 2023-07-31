11d27aa0649b0a4989331742e022b8f68515d87c
Alex Liew <liewtwoo42@gmail.com>
2023-07-31 12:36:47 -0400

    add short names

--- day26/day26.py
+++ day26/day26.py
@@ -6,3 +6,8 @@
 
 # doubled_numbers = [n * 2 for n in range(1, 5)]
 # print(doubled_numbers)
+
+names = ['Alex', 'Beth', "Caroline", 'Dave', 'Eleanor', 'Freddie']
+
+short_names = [short for short in names if len(short) <= 4]
+print(short_names)
