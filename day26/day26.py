e2aec39256d18971ef1be6c65142a7fa00256a9a
Alex Liew <liewtwoo42@gmail.com>
2023-07-31 14:26:54 -0400

    data overlap 237

--- day26/day26.py
+++ day26/day26.py
@@ -15,14 +15,28 @@
 # long_names = [name.upper() for name in names if len(name) > 5]
 
 # print(long_names)
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-# squared_numbers = [num * num for num in numbers]
+# # squared_numbers = [num * num for num in numbers]
 
-# print(squared_numbers)
+# # print(squared_numbers)
 
 
 
-result = [number for number in numbers if number % 2 == 0]
+# result = [number for number in numbers if number % 2 == 0]
+
+# print(result)
+
+file1 = open('file1.txt', 'r')
+file2 = open('file2.txt', 'r')
+x1 = file1.readlines()
+x2 = file2.readlines()
+
+file1_list = [int(num.replace("/n", "")) for num in x1]
+file2_list = [int(num.replace("/n", "")) for num in x2]
+
+result = [number for number in file1_list if number in file2_list]
+
+
 
 print(result)
