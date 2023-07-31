feb23c2867b64fc0e238d7ee4c2527d3047ba1e3
Alex Liew <liewtwoo42@gmail.com>
2023-07-31 14:00:03 -0400

    filtering even numbers 235

--- day26/day26.py
+++ day26/day26.py
@@ -17,6 +17,12 @@
 # print(long_names)
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-squared_numbers = [num * num for num in numbers]
+# squared_numbers = [num * num for num in numbers]
 
-print(squared_numbers)
+# print(squared_numbers)
+
+
+
+result = [number for number in numbers if number % 2 == 0]
+
+print(result)
