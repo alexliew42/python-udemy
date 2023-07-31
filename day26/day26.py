b9b0f03782872315ebe32765474923d671269bff
Alex Liew <liewtwoo42@gmail.com>
2023-07-31 12:33:54 -0400

    short names 234

--- day26/day26.py
+++ day26/day26.py
@@ -4,5 +4,5 @@
 
 # # double the numbers in range(1,5)
 
-doubled_numbers = [n * 2 for n in range(1, 5)]
-print(doubled_numbers)
+# doubled_numbers = [n * 2 for n in range(1, 5)]
+# print(doubled_numbers)
