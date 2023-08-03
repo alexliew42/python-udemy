933180d63827a74ff7005d8230a5998652800b65
Alex Liew <liewtwoo42@gmail.com>
2023-08-02 23:24:14 -0400

    final day 26 commit

--- day26/day26.py
+++ day26/day26.py
@@ -63,16 +63,3 @@
 
 # print(weather_dict)
 
-import pandas
-
-nato_dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-nato_dict = {row.letter:row.code for (index, row) in nato_dataframe.iterrows()}
-
-user_input = input("Enter a word: ")
-
-new_list = [nato_dict[letter.upper()] for letter in user_input]
-
-
-print("This is your code name: ")
-print(new_list)
