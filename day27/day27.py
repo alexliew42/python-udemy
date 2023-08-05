879ca465f7dc5ac50123b0ae9846330539ffff79
Alex Liew <liewtwoo42@gmail.com>
2023-08-04 21:01:53 -0400

    button got clicked tkinter 251

--- day27/day27.py
+++ day27/day27.py
@@ -3,8 +3,23 @@
 #   for number in args:
 #     sum += number
 #   print(sum)
-
-
-
-
 # add(3, 5, 2, 1, 5, 2, 9, 10, 13)
+
+import tkinter
+
+def onClickButton():
+  title["text"] = "button got clicked"
+
+window = tkinter.Tk()
+window.geometry("500x500")
+
+title = tkinter.Label(text = "This is my title.")
+title.pack()
+
+button = tkinter.Button(text = "Click Me!", command = onClickButton)
+button.pack()
+
+
+
+window.mainloop()
+
