ebc85de2c8c4424107bc3825286399dbe689f39c
Alex Liew <liewtwoo42@gmail.com>
2023-08-04 21:50:03 -0400

    miles to km converter project 254

--- day27/day27.py
+++ day27/day27.py
@@ -5,29 +5,59 @@
 #   print(sum)
 # add(3, 5, 2, 1, 5, 2, 9, 10, 13)
 
-import tkinter
+# import tkinter
 
-def onClickButton():
-  title["text"] = user_input.get()
+# def onClickButton():
+#   title["text"] = user_input.get()
 
-window = tkinter.Tk()
-window.geometry("500x500")
+# window = tkinter.Tk()
+# window.geometry("500x500")
 
-title = tkinter.Label(text = "This is my title.")
-title.grid(row = 0, column = 0)
+# title = tkinter.Label(text = "This is my title.")
+# title.grid(row = 0, column = 0)
 
-button = tkinter.Button(text = "Click Me!", command = onClickButton)
-button.grid(row = 1, column = 1)
+# button = tkinter.Button(text = "Click Me!", command = onClickButton)
+# button.grid(row = 1, column = 1)
 
-new_btn = tkinter.Button(text = "no, Click me!")
-new_btn.grid(row=0, column = 3)
-
-
-user_input = tkinter.Entry(width=10)
-user_input.grid(row=2, column=4)
+# new_btn = tkinter.Button(text = "no, Click me!")
+# new_btn.grid(row=0, column = 3)
 
 
+# user_input = tkinter.Entry(width=10)
+# user_input.grid(row=2, column=4)
+
+
+
+
+# window.mainloop()
+
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("400x200")
+window.configure(padx=30, pady=30)
+
+user_input = tk.Entry(width=10)
+user_input.grid(row=0, column=1)
+
+miles = tk.Label(text="Miles")
+miles.grid(row = 0, column=2)
+
+is_equal_to = tk.Label(text="is equal to")
+is_equal_to.grid(row=1, column=0)
+
+output = tk.Label(text="0")
+output.grid(row=1, column = 1)
+
+kilometers = tk.Label(text='km')
+kilometers.grid(row=1, column =2 )
+
+def calculate_to_km():
+  miles = int(user_input.get()) * 1.609
+  output['text'] = miles
+
+calculate = tk.Button(text="Calculate", command= calculate_to_km)
+calculate.grid(row=2, column=1)
 
 
 window.mainloop()
-
