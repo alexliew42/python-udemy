89d0f746876be04201854ee7ede21981cbd604d2
Alex Liew <liewtwoo42@gmail.com>
2023-08-15 16:43:48 -0400

    make corrections

--- day30/day30.py
+++ day30/day30.py
@@ -89,9 +89,11 @@ fruits = ["Apple", "Pear", "Orange"]
 def make_pie(index):
     try:
       fruit = fruits[index]
-      print(fruit + " pie")
-    except: 
+    except IndexError: 
       print('Fruit pie')
+    else: 
+      print(fruit + " pie")
+
 
 
 make_pie(2)
