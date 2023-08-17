5ff4d6fc5e7a71ff691adc6327d27e90441ec70e
Alex Liew <liewtwoo42@gmail.com>
2023-08-16 23:46:10 -0400

    secret code 274

--- day30/main.py
+++ day30/main.py
@@ -4,12 +4,21 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+#TOD 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def secret_code():
+  word = input("Enter a word: ").upper()
+  try:
+    output_list = [phonetic_dict[letter] for letter in word]
+  except KeyError:
+    print("Please enter letters of the alphabet only.")
+    secret_code()
+  else:
+    print(f"Here is your secret code: {output_list}")
+
+
+secret_code()
